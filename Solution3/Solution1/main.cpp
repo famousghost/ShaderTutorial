eff2996bfcaf60a648ba6db42c61597d8d2e532a
Famousghost <mar.czekaj11@gmail.com>
2018-04-04 22:34:17 +0200

    Add Third Solution

---
+++ Solution3/Solution1/main.cpp
@@ -0,0 +1,160 @@
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <Windows.h>
+#include <iostream>
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+
+"layout (location = 1) in vec3 colPos;\n"
+"out vec3 vertexColor;\n"
+"void main()\n"
+"{\n"
+"   vertexColor = colPos;\n"
+"   gl_Position = vec4(aPos,1.0f);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 vertexColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(vertexColor,1.0);\n"
+"}\0";
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "ShaderOpenGL", nullptr, nullptr);
+
+	if (window == nullptr)
+	{
+		std::cerr << "Cannot create window" << std::endl;
+		system("pause");
+		return -1;
+	}
+
+	glewExperimental = GL_TRUE;
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Cannot init glew" << std::endl;
+		system("pause");
+		return -1;
+	}
+
+
+	int width, height;
+	glfwSetKeyCallback(window, key_callback);
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	GLint success;
+	GLchar errbuffer[512];
+
+	GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderId, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShaderId);
+
+	glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShaderId, 512, NULL, errbuffer);
+		std::cerr << errbuffer << std::endl;
+		system("puase");
+		return -1;
+	}
+
+	GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderId, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShaderId);
+
+	glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderId, 512, NULL, errbuffer);
+		std::cerr << errbuffer << std::endl;
+		system("puase");
+		return -1;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShaderId);
+	glAttachShader(shaderProgram, fragmentShaderId);
+
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, errbuffer);
+		std::cerr << errbuffer << std::endl;
+		system("puase");
+		return -1;
+	}
+
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, -0.5f,-0.5f,1.0f,0.5f,
+		0.5f, -0.5f, -0.5f,1.0f,0.0f,0.0f,
+		0.5f,  0.5f, -0.5f,0.0f,1.0f,0.0f,
+		0.5f,  0.5f, -0.5f,1.0f,1.0f,1.0f,
+		-0.5f,  0.5f, -0.5f,0.0f,1.0f,0.5f,
+		-0.5f, -0.5f, -0.5f,0.0f,0.5f,0.0f
+	};
+
+
+
+
+	GLuint VAO, VBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+	}
+
+	return 0;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
